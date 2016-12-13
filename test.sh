9a91775f85a874e96aeca7006c14e79c57fec7a5
user404d <conduffql@gmail.com>
2016-12-13 13:45:08 -0600

    Better fix to travis CI

--- test.sh
+++ test.sh
@@ -7,6 +7,12 @@ docker-compose --file ${YML} stop
 docker-compose --file ${YML} build
 docker-compose --file ${YML} up -d
 
+while ! docker-compose --file ${YML} exec db psql \
+    coliselia --username=postgres -c 'SELECT 1'; do
+  echo 'Waiting for postgres...'
+  sleep 1;
+done;
+
 if ! docker-compose --file ${YML} exec dbapi npm test; then
     docker-compose --file ${YML} stop
     exit 1
