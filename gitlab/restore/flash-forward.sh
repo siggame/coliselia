bfd8b948328b940994e3701377280ce5965455fd
user404d <conduffql@gmail.com>
2016-12-19 18:15:10 -0600

    works, but requires extra step

---
+++ gitlab/restore/flash-forward.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Attempting to restore most recent backup..."
+
+recent=$(ls /var/opt/gitlab/backups | tail -1 | sed "s/_gitlab_backup.tar//g")
+
+gitlab-ctl stop unicorn
+gitlab-ctl stop sidekiq
+
+BACKUP="$recent" && printf "yes\nyes\n" | \
+gitlab-rake gitlab:backup:restore
+
+gitlab-ctl start
